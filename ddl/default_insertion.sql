5a7564b0f8ef19a8aa9dbd105bb6ff1f8ef502d7
NataliaBagnoli <80259327+NataliaBagnoli@users.noreply.github.com>
2022-01-25 15:52:03 +0100

    Create default_insertion.sql

---
+++ ddl/default_insertion.sql
@@ -0,0 +1,8 @@
+INSERT INTO dbtelco.service (typeOfService) VALUES ('Fixed Phone');
+INSERT INTO dbtelco.service (typeOfService, numMinutes, numSMS, feeExtraMinute, feeExtraSMSs) VALUES ('Mobile Phone', '100', '50', '0.5', '0.2');
+INSERT INTO dbtelco.service (typeOfService, numGigabytes, feeForExtraGigabytes) VALUES ('Fixed Internet', '30', '0.7');
+INSERT INTO dbtelco.service (typeOfService, numGigabytes, feeForExtraGigabytes) VALUES ('Mobile Internet', '10', '2');
+
+INSERT INTO dbtelco.validityperiod (numOfMonths, monthlyFee) VALUES ('12', '20');
+INSERT INTO dbtelco.validityperiod (numOfMonths, monthlyFee) VALUES ('24', '18');
+INSERT INTO dbtelco.validityperiod (numOfMonths, monthlyFee) VALUES ('36', '15');
